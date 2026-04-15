70a5b030e935608a1093a2b97afb2eecaeea51dc
BreakingY <kxsun617@163.com>
2026-04-15 17:03:27 +0800

    fix: color

--- view_flo.py
+++ view_flo.py
@@ -1,22 +1,36 @@
-# Copyright (C) 2026 sunkx
-# Licensed under the GNU General Public License v3.0
 import numpy as np
 import cv2
 
 flow = cv2.readOpticalFlow('out.flo')
 
-# 转 HSV 显示
 h, w = flow.shape[:2]
 fx, fy = flow[..., 0], flow[..., 1]
 
 mag, ang = cv2.cartToPolar(fx, fy)
 
 hsv = np.zeros((h, w, 3), dtype=np.uint8)
-hsv[..., 0] = ang * 180 / np.pi / 2
-hsv[..., 1] = 255
-hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+
+ang = np.mod(ang, 2 * np.pi)
+
+hue = np.zeros_like(ang, dtype=np.float32)
+
+mask1 = ang < (np.pi / 2)
+hue[mask1] = (ang[mask1] / (np.pi / 2)) * 30
+
+mask2 = (ang >= np.pi / 2) & (ang < np.pi)
+hue[mask2] = 30 + ((ang[mask2] - np.pi / 2) / (np.pi / 2)) * 90
+
+mask3 = ang >= np.pi
+hue[mask3] = 120 + ((ang[mask3] - np.pi) / np.pi) * 60
+
+hsv[..., 0] = np.clip(hue, 0, 179).astype(np.uint8)
+
+mag_norm = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+hsv[..., 1] = mag_norm.astype(np.uint8)
+
+hsv[..., 2] = 255
 
 rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-cv2.imshow("flow rgb", rgb)
+cv2.imshow("flow semantic rgb", rgb)
 cv2.waitKey(0)
